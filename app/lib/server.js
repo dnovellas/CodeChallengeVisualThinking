daa2da256523801888308b7de4d557fd98a66bfe
David Novella Saldana <dnovella@Novella-Inspiron.localdomain>
2022-05-12 13:09:11 -0500

    Se agrega server.js con los endpoint solicitados

---
+++ app/lib/server.js
@@ -0,0 +1,34 @@
+const VisualPartnersController = require("./controllers/VisualPartnersController");
+// Usando objeto express
+const express = require("express");
+// App de express
+const app = express();
+app.use(express.json());
+// Puerto en que vamos a ver la app: locahost:3000
+const port = 3000;
+
+// path inicial, responderá a la url localhost:3000
+app.get("/", (request, response)=>{
+    response.json({message: "FizzBuzz Api welcome!"});
+});
+
+app.get("/v1/visualpartners", (request, response)=>{    
+    const visualpartners = VisualPartnersController.getAllStudents();
+    response.json(visualpartners);
+});
+
+app.get("/v1/visualpartners/emails/", (request, response)=>{    
+    const visualpartners = VisualPartnersController.getStudentsMailWithCertification();
+    response.json(visualpartners);
+});
+
+app.get("/v1/visualpartners/filterByCredistGreaterThan/:credits", (request, response)=>{
+    const credits = request.params.credits;
+    const filteredByCredits = VisualPartnersController.filterStudentsWithCreditsGreaterThan(credits);    
+    response.json({credits: request.params.credits, visualpartners: filteredByCredits});
+});
+
+//Con esto se inicializa la app
+app.listen(port, ()=> {
+    console.log(`VisualPartners API in localhost:${port}`);
+});
